8eb0a1fe2d59b888e0349d4bb66a3f03e5751a12
ibnabii <someone@somewhere.net>
2024-04-15 11:06:52 +0200

    After video #4

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("join/", views.index),
+    path("join/1/", views.index),
+    path("create/", views.index),
 ]
--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -2,31 +2,35 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "./static/frontend"),
-    filename: "[name].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./static/frontend"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+    },
+    watchOptions: {
+        poll: true,
+        ignored: /node_modules/
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                // This has effect on the react lib size
+                NODE_ENV: JSON.stringify("development"),
+            },
+        }),
     ],
-  },
-  optimization: {
-    minimize: true,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        // This has effect on the react lib size
-        NODE_ENV: JSON.stringify("development"),
-      },
-    }),
-  ],
 };
